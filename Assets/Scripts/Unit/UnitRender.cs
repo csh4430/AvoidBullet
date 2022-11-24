bc7c2d6c0311f620fc29402866d7973d0f4ba249
csh4430 <snghun6889@gmail.com>
2022-11-25 00:55:28 +0900

    render

--- Assets/Scripts/Unit/UnitRender.cs
+++ Assets/Scripts/Unit/UnitRender.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnitRender : UnitBehaviour
@@ -8,12 +9,14 @@ public class UnitRender : UnitBehaviour
     private Material mainMaterial;
     private Material outlineMaterial;
     private Light light;
+    private Color originalColor;
     public override void Awake()
     {
         base.Awake();
         mainMaterial = ThisUnit.transform.Find("Model").GetComponent<MeshRenderer>().material;
         outlineMaterial = ThisUnit.transform.Find("Model/Outline").GetComponent<MeshRenderer>().material;
         light = ThisUnit.transform.Find("Model/Light").GetComponent<Light>();
+        originalColor = outlineMaterial.color;
     }
 
     public override void Start()
@@ -30,9 +33,7 @@ public class UnitRender : UnitBehaviour
     private void LightUnit()
     {
         var percentage = state.Stat.Health / state.Stat.MaxHealth;
-        mainMaterial.color = Color.white * percentage;
-        outlineMaterial.SetFloat("_Alpha", percentage);
-        outlineMaterial.SetFloat("_Intensity", percentage * 2);
+        outlineMaterial.color = originalColor * percentage;
         light.intensity = percentage;
     }
 }
