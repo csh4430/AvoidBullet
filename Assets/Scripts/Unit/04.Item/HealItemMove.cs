8dc9c2b9ce7f6ef43d3081f0ac11206be4b37298
YeEun Oh <OhSolDev@gmail.com>
2022-12-19 23:42:20 +0900

    Edit Heal Item Move

--- Assets/Scripts/Unit/04.Item/HealItemMove.cs
+++ Assets/Scripts/Unit/04.Item/HealItemMove.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class HealItemMove : UnitMove
 {
-    
+    public override void Awake()
+    {
+        base.Awake();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Translate()
+    {
+        base.Translate();
+    }
 }
