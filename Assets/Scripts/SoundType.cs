21ccce32ce19092fd9f290b6f74432055c6c924f
YeEun Oh <OhSolDev@gmail.com>
2022-12-21 01:53:53 +0900

    Add SoundManager

---
+++ Assets/Scripts/SoundType.cs
@@ -0,0 +1,19 @@
+namespace SoundType
+{
+    public enum EffType
+    {
+        //eff name here
+        BtnClick,
+        LoadScene,
+
+    }
+    public enum BgmType
+    {
+        //bgm name here
+        Title,
+        Tuto,
+        MidBoss,
+        FinalBoss
+    }
+    public class SoundType {}
+}
